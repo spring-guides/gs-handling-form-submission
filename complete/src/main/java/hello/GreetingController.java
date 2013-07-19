dbcf75a1abd8603a13dc5596aae45daa52784680
Roy Clarkson <rclarkson@gopivotal.com>
2013-07-19 00:16:44 -0400

    Add initial version of sample code

---
+++ complete/src/main/java/hello/GreetingController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @RequestMapping(value="/greeting", method=RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+    	model.addAttribute("greeting", greeting);
+        return "result";
+    }
+
+}
