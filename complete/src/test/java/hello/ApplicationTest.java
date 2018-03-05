97dbb18684329abb1ddc574a8b52f17cc47258db
Brian Clozel <bclozel@pivotal.io>
2018-03-05 22:02:13 +0100

    Upgrade to Spring Boot 2.0.0.RELEASE

--- complete/src/test/java/hello/ApplicationTest.java
+++ complete/src/test/java/hello/ApplicationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,14 +24,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = "logging.level.org.springframework.web=DEBUG")
-@AutoConfigureMockMvc
+@WebMvcTest(GreetingController.class)
+@TestPropertySource(properties = "logging.level.org.springframework.web=DEBUG")
 public class ApplicationTest {
 
     @Autowired
