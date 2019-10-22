8032014f42f6384f9a371db4ed5ce02d3d62060f
Jay Bryant <jbryant@pivotal.io>
2019-10-22 15:29:36 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the test to work with JUnit 5 and replacing the Gradle wrappers.

--- complete/src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTest.java
+++ complete/src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTest.java
@@ -21,15 +21,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(GreetingController.class)
 @TestPropertySource(properties = "logging.level.org.springframework.web=DEBUG")
 public class HandlingFormSubmissionApplicationTest {
