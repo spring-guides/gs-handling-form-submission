bedd60bbe0ebb0852149f3aeb3bf71ce2e18da5e
Jay Bryant <jbryant@pivotal.io>
2019-10-22 15:29:36 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the test to work with JUnit 5 and replacing the Gradle wrappers.

---
+++ initial/src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handlingformsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingFormSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
