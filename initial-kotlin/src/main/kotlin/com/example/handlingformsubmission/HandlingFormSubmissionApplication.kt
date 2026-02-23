c6c6eacdab0c62e21eae47976f5d00937cbc6d98
Danil Pavlov <danil.pavlov@jetbrains.com>
2026-02-23 14:14:51 +0100

    Add Kotlin projects
    Signed-off-by: Danil Pavlov <danil.pavlov@jetbrains.com>

---
+++ initial-kotlin/src/main/kotlin/com/example/handlingformsubmission/HandlingFormSubmissionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.handlingformsubmission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HandlingFormSubmissionApplication
+
+fun main(args: Array<String>) {
+    runApplication<HandlingFormSubmissionApplication>(*args)
+}
